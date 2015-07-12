7eb495b5f99a9adf126ca9a90b1d9ed14b8d1173
Cody <meanderingmonarch@gmail.com>
2015-07-12 12:40:48 -0500

    Added the game logic flow to game.js

--- js/game.js
+++ js/game.js
@@ -10,3 +10,36 @@
     var game = this.colorQuestGame =
         this.colorQuestGame || {};
 })();
+
+
+//Main game logic
+game.flow = {
+    startOver: function(){
+        game.startScene.hide();
+        game.summaryScene.hide();
+        
+        game.gameoverScene.hide();
+        game.gameScene.hide();
+        game.startScene.show();
+    },
+    gameWin: function(){
+        game.gameScene.hide();
+        
+        game.summaryScene.show();
+    },
+    gameOver: function(){
+        game.startScene.show();
+        game.gameScene.hide();
+        
+        game.gameoverScene.show();
+    },
+    nextLevel: function(){
+        game.startScene.hide();
+        game.summaryScene.show();
+        game.gameScene.show();
+    },
+    finishLevel: function(){
+        game.gameScene.hide();
+        game.summaryScene.show();
+    },
+}
